61892b0a77ba95a0b5c62d41f9ed59017e01b311
Crystal Korth <crystalkorth@gmail.com>
2022-06-21 20:37:56 -0500

    working out a few bugs

--- public/ingredientList.js
+++ public/ingredientList.js
@@ -4,24 +4,24 @@ const ingredientList = {
 ingredients:  [],
 
 // Updates the DOM to display a list of ingredients
-renderIngredientList: function () {
+render() {
     const ingredientList = document.querySelector('.ingredient-list');
 
     // Empty the ingredientList before adding any content to it.
-    ingredientUl.innerHTML = '';
+    ingredientList.innerHTML = '';
 
-    ingredients.forEach(ingredient => {
-        const ingredientDiv = renderIngredientCard(ingredient);
-        ingredientUl.append(ingredientDiv)
+    this.ingredients.forEach(ingredient => {
+       const ingredientDiv = this.createIngredientCard(ingredient);
+        ingredientList.append(ingredientDiv)
     })
-} }
+}, 
 
 // Creates a DIV to display a single ingredient
-function renderIngredientCard(ingredient) {
+createIngredientCard(ingredient) {
     const ingredientCard = document.createElement('div');
     ingredientCard.className = 'card'
 
-    const sandwichHasIngredient = selectedSandwich.ingredients.includes(ingredient.name);
+    const sandwichHasIngredient = cart.selectedSandwich.ingredients.includes(ingredient.name);
     ingredientCard.innerHTML = `
         <div class="card-body">
             <div class="row g-0">
@@ -43,22 +43,23 @@ function renderIngredientCard(ingredient) {
     `
     const toggleButton = ingredientCard.querySelector('.toggle-button')
     toggleButton.addEventListener('click', () => {
-        toggleIngredient(ingredient)
+        ingredientList.toggleIngredient(ingredient)
     })
 
     return ingredientCard
-}
+},
 
 // Runs when the user clicks 'Add' or 'Remove' on a ingredient card
-function toggleIngredient(ingredient) {
-    let sandwichHasIngredient = selectedSandwich.ingredients.includes(ingredient.name);
+toggleIngredient(ingredient) {
+    let sandwichHasIngredient = cart.selectedSandwich.ingredients.includes(ingredient.name);
     if (sandwichHasIngredient) {
-        selectedSandwich.ingredients = selectedSandwich.ingredients.filter(x => x !== ingredient.name)
+        cart.selectedSandwich.ingredients = cart.selectedSandwich.ingredients.filter(x => x !== ingredient.name)
     } else {
         sandwichHasIngredient = true;
-        selectedSandwich.ingredients.push(ingredient.name)
+        cart.selectedSandwich.ingredients.push(ingredient.name)
     }
-    saveSelectedSandwich()
-    renderCart()
-    renderIngredientList()
+    cart.saveSelectedSandwich()
+    cart.render()
+    ingredientList.render()
+}
 }
